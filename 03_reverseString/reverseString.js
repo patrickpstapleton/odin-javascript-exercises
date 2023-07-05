cbb18c251a744437534f3dc456ce1c99ad3a9222
Patrick Stapleton <132307108+patrickpstapleton@users.noreply.github.com>
2023-07-05 16:45:48 -0400

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let inputLength = inputString.length
+  console.log(inputLength)
+  let splitArray = [];
+  for (let i = 0; i < inputLength; i++) {
+    splitArray = splitArray.concat(inputString.charAt((inputLength - 1) - i));
+  }
+  return splitArray.join("")
 };
 
 // Do not edit below this line
