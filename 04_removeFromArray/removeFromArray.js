019da97b3386f331e2e7392dda7c061e50fb7e39
Patrick Stapleton <132307108+patrickpstapleton@users.noreply.github.com>
2023-07-05 17:23:37 -0400

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(baseArray, ...badApples) {
+  return baseArray.filter(item => testApples(item, badApples));
 };
+
+function testApples(item, badApples) {
+  let index = 0;
+  let bad = false;
+  while (index < badApples.length && bad == false) {
+    bad = item === badApples[index]
+    index++
+  }
+  return !bad
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
