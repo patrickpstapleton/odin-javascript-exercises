ae81532ce699cbb62e23c73cce28692cf00d8726
Patrick Stapleton <132307108+patrickpstapleton@users.noreply.github.com>
2023-07-09 14:55:07 -0400

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+  let sum = 0;
+  let start = firstNum;
+  let end = secondNum;
+
+  if (firstNum < 0 || secondNum < 0 || 
+      typeof firstNum !== 'number' || 
+      typeof secondNum !== 'number') {
+    return 'ERROR';
+  }
+  else if(firstNum > secondNum) {
+    start = secondNum;
+    end = firstNum;
+  }
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
